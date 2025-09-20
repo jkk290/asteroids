74d8e337462568463293a763d79c5275b51f2772
jkk290 <jared@kuromoto.dev>
2025-09-19 21:54:03 -1000

    Update asteroids to split on hit

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
             for shot in shots:
                 if asteroid.collision(shot):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill("black")
         for item in drawable:
